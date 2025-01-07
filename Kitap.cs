7ca821339ddeb5d86091bb64c25ced295c04c55c
osmanozysr <123673331+OsmanOzyasar@users.noreply.github.com>
2025-01-07 18:07:10 +0300

    Add project files.

---
+++ Kitap.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutuphane
+{
+    public class Kitap
+    {
+        public Kitap() { } //Constructor, bir sınıfın örneği oluşturulurken otomatik olarak çalışan, sınıfın başlangıç değerlerini ayarlayan özel bir metottur.
+
+        private string? _kitapAdı;
+        private string? _kitapYazarı;
+        private string? _yazarSoyadı;
+        private string? _sayfa;
+        private string? _yayınevi;
+        public static DateTime _kayıtZamanı = DateTime.Now;
+
+        public string? KitapAdı { get { return _kitapAdı; } set { _kitapAdı = value; } } //Property, bir sınıfın alanlarını (field) dışarıya güvenli bir şekilde açan ve okuma/yazma işlemleri yapan özel bir özelliktir.
+        public string? KitapYazarı { get { return _kitapYazarı; } set { _kitapYazarı = value; } }
+        public string? YazarSoyad { get { return _yazarSoyadı; } set { _yazarSoyadı = value; } }
+        public string? Sayfa { get { return _sayfa; } set { _sayfa = value; } }
+        public string? YayınEvi { get { return _yayınevi; } set { _yayınevi = value; } }
+        public Kitap(string kitapAdı, string kitapYazarı, string yazarSoyadı, string sayfa, string yayınevi)
+        {
+            _kitapAdı = kitapAdı;
+            _kitapYazarı = kitapYazarı;
+            _yazarSoyadı = yazarSoyadı;
+            _sayfa = sayfa;
+            _yayınevi = yayınevi;
+
+            Console.WriteLine($"{kitapAdı} /{kitapYazarı} {yazarSoyadı}/ {sayfa} sayfa / {yayınevi} yayınevi");
+        }//Constructor, bir sınıfın örneği oluşturulurken otomatik olarak çalışan, sınıfın başlangıç değerlerini ayarlayan özel bir metottur.
+
+        public void KonsolaYazdır()
+        {
+            Console.WriteLine($"{_kitapAdı} /{_kitapYazarı} {_yazarSoyadı}/ {_sayfa} sayfa / {_yayınevi} yayınevi");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Kutuphane;
+using System;
+using System.Collections.Generic;
+
+namespace KitapKayıt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kitap kitap = new Kitap("Sefiller", "Viktor", "Hugo", "500", "İş Bankası");//New anahtar kelimesi, bir sınıfın yeni bir örneğini (instance) oluşturmak için kullanılır.
+
+            DateTime kayıt = Kitap._kayıtZamanı;
+            Console.WriteLine("Kayıt zamnı: " + kayıt);
+
+            Kitap kitap1 = new Kitap();
+
+            kitap1.KitapAdı = "Savaş ve Barış";
+            kitap1.KitapYazarı = "Lev";
+            kitap1.YazarSoyad = "Tolstoy";
+            kitap1.Sayfa = "1440";
+            kitap1.YayınEvi = "İş Bankası";
+
+            kitap1.KonsolaYazdır();
+
+            DateTime kayıt2 = Kitap._kayıtZamanı;
+            Console.WriteLine("Kayıt zamnı: " + kayıt2);
+        }
+    }// class, bir nesnenin özelliklerini ve davranışlarını tanımlayan bir şablondur
+}
